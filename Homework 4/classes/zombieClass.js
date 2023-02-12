a305bba22f2ebc85b35202d7f72bb9c78325c498
JKulpas <112431920+JKulpas@users.noreply.github.com>
2023-02-12 12:25:31 -0700

    Update zombieClass.js

--- Homework 4/classes/zombieClass.js
+++ Homework 4/classes/zombieClass.js
@@ -1,1 +1,11 @@
-
+class zombie{
+  
+  constructor(height, weight, age, speed) {
+    
+    this.height = height;
+    this.weight = weight;
+    this.age = weight;
+    this.speed = speed;
+  }
+  
+}
