b035535e0fa0f299f309a4a1b00c75102c658f37
JKulpas <112431920+JKulpas@users.noreply.github.com>
2023-02-13 19:47:59 -0700

    Update zombieClass.js

--- Homework 4/classes/zombieClass.js
+++ Homework 4/classes/zombieClass.js
@@ -1,11 +1,70 @@
-class zombie{
-  
-  constructor(height, weight, age, speed) {
-    
-    this.height = height;
-    this.weight = weight;
-    this.age = weight;
-    this.speed = speed;
+class zombie {
+    constructor(path, x, y) 
+  {
+    this.path = path;
+    this.x = x;
+    this.y = y;
+}
+
+getX()
+{
+    return this.x;
+}
+
+getY()
+{
+    return this.y;
+}
+
+idle()
+{
+  image(zombieObjects[i], x, y, 100, 200);
+
+  i += 1;
+  if (i >= zombieObjects.length) {
+      i = 0;
   }
-  
+}
+ movement()
+ {
+  if(keyIsPressed)
+    {
+        if(key == "w")
+        {
+            y-=10
+        }
+        else if(key == "a" )
+        {
+            x-=10
+        }
+        else if(key == "s")
+        {
+            y+=10
+        }
+        else if (key == "d")
+        {
+            x+=10
+        }
+        else
+        {
+            image(boyZombie[i], zombieObjects[i].getX(), zombieObjects[i].getY());
+        }
+ }
+}
+run()
+{
+
+    j += 1;
+
+    if (j >= zombieRunArray.length) 
+    {
+        j = 0;
+    }
+}
+}
+
+class iceCream
+{
+constructor()
+
 }
