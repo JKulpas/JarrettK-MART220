610f6f5287888b80ad659436f9cfb8b2bb509c99
JKulpas <112431920+JKulpas@users.noreply.github.com>
2023-03-06 19:55:23 -0700

    Update iceCreamClass.js

--- Homework 7/js/iceCreamClass.js
+++ Homework 7/js/iceCreamClass.js
@@ -1,24 +1,42 @@
 class iceCreamFood {
-    constructor(x, y)
+    constructor(x, y, w, h)
     {
         this.x = x;
         this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+    getX()
+    {
+      return this.x;
+    }
+    getY()
+    {
+      return this.y;
+    }
+    getW()
+    {
+      return this.w;
+    }
+    getH()
+    {
+      return this.h;
     }
 
  drawIceCream(){
         noStroke();
         fill(128,75,18)
-        ellipse(this.x,this.y-60,70)
+        ellipse(this.x,this.y-60, 70, 70)
         fill(255,255,255)
-        ellipse(this.x,this.y-30,75)
+        ellipse(this.x,this.y-30, 75, 75)
         fill(255,153,180)
-        ellipse(this.x,this.y,80)
+        ellipse(this.x,this.y,this.w, this.h)
         
       
         fill(205,133,63)
         stroke(1);
         strokeWeight(0.3);
-        triangle(this.x-41,this.y+6,this.x+41,this.y+6,this.x,this.y+90)
+        triangle(this.x-41,this.y+6,this.x+41,this.y+6,this.x,this.y+90,)
         
     }
 }
