dc4226bd21b6dec361019bc3f01e7cb0f84ba9d3
JKulpas <112431920+JKulpas@users.noreply.github.com>
2023-03-13 09:47:40 -0600

    Create myCollision.js

---
+++ Homework 7/js/myCollision.js
@@ -0,0 +1,33 @@
+class myRectangle
+{
+  
+  constructor(x,y,w,h)
+  {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+  
+  getX()
+  {
+    return this.x;
+  }
+  getY()
+  {
+    return this.y;
+  }
+  getW()
+  {
+    return this.w;
+  }
+  getH()
+  {
+    return this.h;
+  }
+  
+  draw()
+  {
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
